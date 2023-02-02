9d1c7171d264d4bfad0325df20e424d5ec06d595
DerickLFAndrade <derickspy15@gmail.com>
2023-02-02 01:19:41 -0300

    temporizador

--- JavaScript.js
+++ JavaScript.js
@@ -1,12 +1,22 @@
 let a = document.getElementById("cont");
+let n = document.getElementById("number");
 let bool;
+let i = 0;
+window.onload = ()=>{
+    setInterval(() => {
+        n.innerHTML = i++
+    }, 1000);
+}
 a.addEventListener("click", ()=>{
     if(bool == false) {
         a.style.background = "#f00";
         bool = true;
+      
     }
     else {
         a.style.background = "#ccc";
         bool = false;
     }
 })
+
+       
